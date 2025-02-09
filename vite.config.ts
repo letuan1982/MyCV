69fa1cac8032b21eab2e7b1f43c847a055e6f88c
letuan1982 <letranquoctuan@gmail.com>
2025-02-09 21:13:55 +0700

    Update vite.config.ts for deploying on Netlify

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,11 @@
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/',
+  base: '/', // Đảm bảo đường dẫn đúng
+  build: {
+    outDir: 'dist', // Thư mục xuất file build
+    assetsDir: 'assets', // Đảm bảo đúng cấu trúc file
+  },
 })
